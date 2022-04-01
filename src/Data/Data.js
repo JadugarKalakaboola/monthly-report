e20075e50c96fc90b04f22674b398df6fae27362
Siddharth <sid.sharma1704@gmail.com>
2022-04-01 23:00:08 +0530

    Setup

---
+++ src/Data/Data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        month : "January",
+        year : "2022",
+        qoute : "",
+        progress : [
+            "Started Intermediate Javascript",
+            "Won ZeroDay competition",
+            "Learned about github"
+        ],
+    },
+    {
+        month : "February",
+        year : "2022",
+        qoute : "",
+        progress : [
+            "Went ahead towards advance javascript",
+            "Improved my styling skills",
+            "Did some FrontendMentor challenges"
+        ],
+    },
+    {
+        month : "March",
+        year : "2022",
+        qoute : "",
+        progress : [
+            "ES6+ Javascript",
+            "Started React ✨",
+            "Learned Basic React"
+        ],
+    }
+]
